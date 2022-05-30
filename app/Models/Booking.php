f1e5ea4131a4b8766f7523aa076aa98ccc3af2f3
augusto <augusto.perceval@est.fi.uncoma.edu.ar>
2022-05-30 17:17:23 -0300

    ORM booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -17,4 +17,27 @@ class Booking extends Model
     'start_time', 
     'finish_time'];
 
+        //ORM una reserva tiene un usuario
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'foreign_key', 'local_key');
+    }
+
+   //ORM una reserva tiene un evento
+    public function event(): HasOne
+    {
+        return $this->hasOne(event::class, 'foreign_key', 'local_key');
+    }
+
+    //ORM una reserva tiene un aula
+    public function classroom(): HasOne
+    {
+        return $this->hasOne(classroom::class, 'foreign_key', 'local_key');
+    }
+
+    //ORM se marca bidireccionalidad (chequear si es correcto)
+    public function assignment():belongsTo {
+        return $this->belongsTo(assignment::class, 'foreign_key', 'local_key');
+    }
+
 }
