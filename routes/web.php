2f4bc364af38b2c69762e240c5b013423f5a597c
Matias <maty.infante@gmail.com>
2022-05-29 22:01:55 -0300

    update .gitignore

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-require __DIR__.'/auth.php';
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
