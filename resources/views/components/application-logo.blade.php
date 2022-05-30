8285bfbe42098006eb81da4e63194742b8b505ad
mistchenco <augusto.perceval@est.fi.uncoma.edu.ar>
2022-05-29 21:50:40 -0300

    auth bootstrap

--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{asset('assets/img/aulario.png')}}" style="height: 60px">
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
