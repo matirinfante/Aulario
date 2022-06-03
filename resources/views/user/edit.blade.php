947f3186c45db1ad620246fb14191abecb8f59bf
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-03 18:47:01 -0300

    minor fix at User views || general yields at layout

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,64 +1,66 @@
 @extends('layouts.app')
+@section('styles')
+    <style>
+        form {
+            width: 40%;
+            min-width: 500px;
+            margin: auto;
+            background: #D9D9D9;
+            padding: 25px;
+            border-radius: 25px;
+            text-align: center;
+        }
 
+        label {
+            font-weight: 600;
+        }
+
+        .alerta {
+            padding: 3px;
+            font-size: 14px;
+            color: #842029;
+            background: #f8d7da;
+            border: solid 1px #f5c2c7;
+            border-radius: 5px;
+            margin-top: 3px;
+            width: 100%;
+        }
+    </style>
+@endsection
 
 @section('content')
-<style>
-    form {
-        width: 40%;
-        min-width: 500px;
-        margin: auto;
-        background: #D9D9D9;
-        padding: 25px;
-        border-radius: 25px;
-        text-align: center;
-    }
+    <a class="btn btn-outline-light" href="{{route('users.index')}}" role="button" style="margin-left: 1%">Listado de
+        Usuarios</a>
 
-    label {
-        font-weight: 600;
-    }
+    <h3 class="text-center m-4">Modificación de un usuario</h3>
+    <form id="form" name="form" class="form_style" method="post" action="{{route('users.update', $user['id'])}}">
+        @method('PATCH')
+        <div class="mb-3">
+            <label for="name" class="form-label">Nombre</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder=" {{$user['name']}} ">
+            <p class="alerta d-none" id="errorName">Error</p>
+        </div>
+        <div class="mb-3">
+            <label for="surname" class="form-label">Apellido</label>
+            <input type="text" class="form-control" name="surname" id="surname" placeholder="{{$user['surname']}}">
+            <p class="alerta d-none" id="errorSurname">Error</p>
+        </div>
+        <div class="mb-3">
+            <label for="dni" class="form-label">Dni</label>
+            <input type="number" class="form-control" name="dni" id="dni" min="1000000" max="99999999"
+                   placeholder="{{$user['dni']}}">
+            <p class="alerta d-none" id="errorDni">Error</p>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="{{$user['email']}}">
+            <p class="alerta d-none" id="errorEmail">Error</p>
+        </div>
 
-    .alerta {
-        padding: 3px;
-        font-size: 14px;
-        color: #842029;
-        background: #f8d7da;
-        border: solid 1px #f5c2c7;
-        border-radius: 5px;
-        margin-top: 3px;
-        width: 100%;
-    }
-
-</style>
-<a class="btn btn-outline-light" href="{{ url('users') }}" role="button" style="margin-left: 1%">Listado de Usuarios</a>
-    
-<h3 class="text-center m-4">Modificación de un usuario</h3>
-<form id="form" name="form" class="form_style" method="get" action="{{route('users.update', $user['id'])}}">
-    <div class="mb-3">
-        <label for="name" class="form-label">Nombre</label>
-        <input type="text" class="form-control" name="name" id="name" placeholder=" {{$user['name']}} ">
-        <p class="alerta d-none" id="errorName">Error</p>
-    </div>
-    <div class="mb-3">
-        <label for="surname" class="form-label">Apellido</label>
-        <input type="text" class="form-control" name="surname" id="surname" placeholder="{{$user['surname']}}">
-        <p class="alerta d-none" id="errorSurname">Error</p>
-    </div>
-    <div class="mb-3">
-        <label for="dni" class="form-label">Dni</label>
-        <input type="number" class="form-control" name="dni" id="dni" min="1000000" max="99999999"
-               placeholder="{{$user['dni']}}">
-        <p class="alerta d-none" id="errorDni">Error</p>
-    </div>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" name="email" id="email" placeholder="{{$user['email']}}">
-        <p class="alerta d-none" id="errorEmail">Error</p>
-    </div>
-    
-    <button id="submit" type="submit" class="btn btn-primary disabled" >Actualizar</button>
-    @csrf
-</form>
-<script src="https://unpkg.com/validator@latest/validator.min.js"></script>
+        <button id="submit" type="submit" class="btn btn-primary disabled">Actualizar</button>
+        @csrf
+    </form>
+    <script src="https://unpkg.com/validator@latest/validator.min.js"></script>
     <script>
         let d = document
         const $inputName = d.getElementById('name'),
@@ -135,11 +137,11 @@
                 $errorEmail.textContent = 'El email está vacio'
                 $errorEmail.classList.remove('d-none')
             }
-            
+
             if (v1 && v2 && v3 && v4) {
                 $button.classList.remove('disabled')
             }
         })
     </script>
-    
+
 @endsection
