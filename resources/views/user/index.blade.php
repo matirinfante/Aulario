1b82d0357a4cf77b3d3a56ce2590a63614b67791
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-04 09:57:53 -0300

    error handling on Assignment Controller

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-        
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
 
-<div class="card" style="width: 1000px; margin: auto;">
-    <div class="card-body">
-        <table class="table table-striped table-hover" id="users">
-            <thead class="bg-secondary text-light">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+
+    <div class="card" style="width: 1000px; margin: auto;">
+        <div class="card-body">
+            <table class="table table-striped table-hover" id="users">
+                <thead class="bg-secondary text-light">
                 <tr>
                     <td>Nombre</td>
                     <td>Apellido</td>
@@ -15,40 +15,40 @@
                     <td>Email</td>
                     <td>Accion</td>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @forelse ($users as $user)
-                <tr>
-                    <td> {{$user['name']}} </td>
-                    <td>{{$user['surname']}}</td>
-                    <td>{{$user['dni']}}</td>
-                    <td>{{$user['email']}}</td>
-                    <td> 
-                        <a class="link-primary" 
-                        href="{{route('users.show', $user['id'])}}"
-                        >Ver</a>
-                        <a class="link-success" 
-                        href=" {{route('users.edit', $user['id'])}}"
-                        >Editar</a>
-                        <a class="link-danger buttonDelete"  href="">Borrar</a>
-                    </td>
-                </tr>
+                    <tr>
+                        <td> {{$user['name']}} </td>
+                        <td>{{$user['surname']}}</td>
+                        <td>{{$user['dni']}}</td>
+                        <td>{{$user['email']}}</td>
+                        <td>
+                            <a class="link-primary"
+                               href="{{route('users.show', $user['id'])}}"
+                            >Ver</a>
+                            <a class="link-success"
+                               href=" {{route('users.edit', $user['id'])}}"
+                            >Editar</a>
+                            <a class="link-danger buttonDelete" href="">Borrar</a>
+                        </td>
+                    </tr>
                 @empty
-                    <td> No hay registros </td>
+                    <td>No hay registros</td>
                     <td>No hay registros</td>
                     <td>No hay registros</td>
                     <td>No hay registros</td>
                 @endforelse
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
-<div class="modal" id="deleteModal" tabindex="-1">
+    <div class="modal" id="deleteModal" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <form id="form_user_delete" name="form_user_delete" method="POST">
-                        @csrf
+                    @csrf
                     <div class="modal-header">
                         <h5 class="modal-title">Eliminación de Usuario</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -60,26 +60,26 @@
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-danger">Borrar</button>
                     </div>
+                </form>
             </div>
-            </form>
         </div>
     </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap5.min.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#users').DataTable();
-                $(document).on('click', '.buttonDelete', function(e) {
-                    e.preventDefault();
-                    var id = $(this).data('value');
-                    $('#userId').val(id);
-                    $('#deleteModal').modal('show');
-                });
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#users').DataTable();
+            $(document).on('click', '.buttonDelete', function (e) {
+                e.preventDefault();
+                var id = $(this).data('value');
+                $('#userId').val(id);
+                $('#deleteModal').modal('show');
             });
-        </script>
+        });
+    </script>
 @endsection
 
 
