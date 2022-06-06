6373831ff6c0f13b93ae9a568dc37f7e734cbb5e
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-06 18:09:32 -0300

    sending soft deleted users to index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::withTrashed()->get();
         return view('user.index', compact('users'));
     }
 
