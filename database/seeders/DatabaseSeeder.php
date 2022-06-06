d87b0efe22403ca1124747f4821120f4c3d624c4
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-05 23:33:58 -0300

    another hotfix for seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -116,8 +116,7 @@ class DatabaseSeeder extends Seeder
         Assignment::factory(10)->create();
         Petition::factory(10)->create();
 
-        User::find(11)->assignments()->sync(Classroom::find(2));
-        User::find(11)->assignments()->sync(Classroom::find(3));
+        User::find(11)->assignments()->sync([2,3]);
         User::find(1)->assignments()->sync(Classroom::find(2));
         User::find(2)->assignments()->sync(Classroom::find(4));
 
