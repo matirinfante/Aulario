1de50e627424fe0002beb055d8e7284768bdbf6b
Cristhian Cantero <cristhian.cantero@init-dev.com.ar>
2022-06-07 17:23:20 -0300

    Modales para crear, modificar y ver evento.

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -3,22 +3,16 @@
 @section('content')
     <div class="container">
         <h3 class="text-center m-4">Crear Evento</h3>
-        <form action="" method="post" class="form_style col-md-4">
+        <form action="{{ route('events.store') }}" method="POST" class="form_style col-md-4" id="form_event"
+            name="form_event">
+            @csrf
             <div class="mb-3">
-                <label for="nombreAula" class="form-label">Nombre Aula</label>
-                <input type="text" class="form-control" id="nombreAula" placeholder="I1">
+                <label for="event_name" class="form-label">Nombre Evento</label>
+                <input type="text" class="form-control" id="event_name" name="event_name" placeholder="Parcial PWA">
             </div>
             <div class="mb-3">
-                <label for="locacionFacultad" class="form-label">Locacion Facultad</label>
-                <input type="text" class="form-control" id="locacionFacultad" placeholder="Planta Alta">
-            </div>
-            <div class="mb-3">
-                <label for="capacidad" class="form-label">Capacidad</label>
-                <input type="number" class="form-control" id="capacidad" placeholder="150">
-            </div>
-            <div class="mb-3">
-                <label for="tipoAula" class="form-label">Tipo de Aula</label>
-                <input type="text" class="form-control" id="tipoAula" placeholder="Laboratorio">
+                <label for="participants" class="form-label">Participantes</label>
+                <input type="number" class="form-control" id="participants" name="participants" placeholder="50">
             </div>
             <button type="submit" class="btn btn-primary">Crear</button>
         </form>
