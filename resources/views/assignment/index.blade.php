60ecf8dadd33557b0b691653754b340e464eb5de
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-06-08 02:21:31 -0300

    implemented ajax to enable assignment

--- resources/views/assignment/index.blade.php
+++ resources/views/assignment/index.blade.php
@@ -211,18 +211,9 @@
                             </div>
 
 
-
-                            {{-- delete assignment button (form) --}}
-                            {{-- <form method="POST" class="form-delete d-inline"
-                                action="{{ route('assignments.destroy', $assignment->id) }}">
-                                @method('DELETE')
-                                @csrf --}}
-                            {{-- <button data-assignment="{{ $assignment->assignment_name }}" type="submit"
-                                    class="btn btn-outline-danger btn-sm">Cambiar estado</button> --}}
-
-                            {{-- </form> --}}
                         </td>
                         <td>
+                            {{-- Cambiar Cuatrimestre de materia --}}
                             <form method="POST" class="form-delete d-inline"
                                 action="">
                                 @method('PUT')
@@ -233,6 +224,7 @@
                             </form>
                         </td>
                         <td>
+                            {{-- Habilitar/Deshabilitar materia (botón switch) --}}
                             <div class="form-check form-switch">
                                 <input data-id="{{ $assignment->id }}" data-token="{{ csrf_token() }}"
                                     class="form-check-input activeSwitch" type="checkbox" role="switch"
@@ -310,12 +302,11 @@
 </script>
 
 
+{{-- Habilitar/Deshabilitar una materia --}}
 <script>
     $('.activeSwitch').change(function(e) {
         var $id = $(this).data('id');
         var status = $(this).prop('checked') == true ? 1 : 0;
-        console.log($id);
-        console.log(status);
 
         if (status == 0) { // deshabilitar materia
             var url = '{{ route('assignments.destroy', ':id') }}';
@@ -331,8 +322,9 @@
                     "_token": token,
                 },
                 success: function(data) {
+                    console.log('entra DELETE');
+                    console.log(data);
                     var $elementSvg = $("td[data-statusSvg='" + $id + "']");
-                    // console.log($elementSvg[0]);
                     $elementSvg.replaceWith('<td class="text-secondary" data-statussvg="' + $id +
                         '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#9b0808" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" /><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" /></svg></td>'
                     );
@@ -362,6 +354,49 @@
             });
         } else {
             // habilitar materia...
+            var url = '{{ route('assignments.activate', ':id') }}';
+            url = url.replace(':id', $id);
+            var token = $(this).data("token");
+            $.ajax({
+                type: 'POST',
+                url: url,
+                cache: false,
+                data: {
+                    "id": $id,
+                    "_method": 'PUT',
+                    "_token": token
+                },
+                success: function(data) {
+                    // console.log('entra PUT');
+                    // console.log(data);
+                    var $elementSvg = $("td[data-statusSvg='" + $id + "']");
+                    $elementSvg.replaceWith('<td class="text-secondary" data-statussvg="' + $id +
+                        '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1f9b08" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" /><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" /></svg></td>'
+                    );
+
+                    $('#flashMessage').html(
+                            '<div class="alert alert-success">Materia habilitada correctamente</div>')
+                        .delay(1000);
+                    var flash = $('#flashMessage');
+                    if (flash.find('.alert.alert-success').length > 0) {
+                        var timerInterval
+                        Swal.fire({
+                            toast: true,
+                            position: 'bottom-end',
+                            background: '#a5dc86',
+                            color: '#000',
+                            showConfirmButton: false,
+                            html: 'Materia habilitada con éxito.',
+                            timer: 2000,
+                            timerProgressBar: true,
+                            willClose: () => {
+                                clearInterval(timerInterval)
+                            }
+                        })
+                    }
+                }
+
+            });
         }
     });
 </script>
@@ -391,24 +426,6 @@
 
 
 <script>
-    // $('.form-delete').submit(function(e) {
-    //     e.preventDefault();
-    //     Swal.fire({
-    //         title: '¿Está seguro de que desea cambiar el estado de la materia ' + $(this).children(
-    //             ':nth-child(3)').data('assignment') + '?',
-    //         icon: 'warning',
-    //         showCancelButton: true,
-    //         confirmButtonColor: '#0a58ca',
-    //         cancelButtonColor: '#b02a37',
-    //         confirmButtonText: 'Cambiar',
-    //         cancelButtonText: 'Cancelar'
-    //     }).then((result) => {
-    //         if (result.isConfirmed) {
-    //             this.submit();
-    //         }
-    //     })
-    // });
-
 
     $(document).ready(function() {
         var flash = $('#flashMessage');
