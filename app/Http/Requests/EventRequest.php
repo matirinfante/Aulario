ba7029204cdd680564b55772937ffe36dd45fa8e
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2022-06-08 12:42:00 -0300

    update: authorize method

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -13,7 +13,7 @@ class EventRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/PetitionRequest.php
+++ app/Http/Requests/PetitionRequest.php
@@ -13,7 +13,7 @@ class PetitionRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
