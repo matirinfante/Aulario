c7a1bdafd2acb498393748cc9c6cefc98827a4cd
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-09 16:24:03 -0300

    adding soft delete to event

--- database/migrations/2022_05_28_183156_create_events_table.php
+++ database/migrations/2022_05_28_183156_create_events_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->id(); //event_id
             $table->string('event_name'); //Nombre del evento
             $table->integer('participants'); //Participantes
+            $table->softDeletes();
             $table->timestamps();
         });
     }
