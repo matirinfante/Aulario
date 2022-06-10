991c28e35c64e9f2d4aa63e73ba9560aba80d5ed
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-09 23:43:34 -0300

    adding soft delete to event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -16,7 +16,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        $events = Event::all();
+        $events = Event::withTrashed()->get();
         return view('event.index', compact('events'));
     }
 
