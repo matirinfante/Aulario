ff3a15463e8a0a76304b9e1986151bba8fa9078b
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-12 20:32:10 -0300

    adding some missing fields at Booking fillable

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,6 +10,10 @@ class Booking extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
+        'classroom_id',
+        'event_id',
+        'assignment_id',
         'description',
         'status',
         'week_day',
@@ -20,25 +24,25 @@ class Booking extends Model
     //ORM una reserva tiene un usuario
     public function user()
     {
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     //ORM una reserva tiene un evento
     public function event()
     {
-        return $this->belongsTo(Event::class,'event_id');
+        return $this->belongsTo(Event::class, 'event_id');
     }
 
     //ORM una reserva tiene un aula
     public function classroom()
     {
-        return $this->belongsTo(Classroom::class,'classroom_id');
+        return $this->belongsTo(Classroom::class, 'classroom_id');
     }
 
     //ORM se marca bidireccionalidad
     public function assignment()
     {
-        return $this->belongsTo(Assignment::class,'assignment_id');
+        return $this->belongsTo(Assignment::class, 'assignment_id');
     }
 
 }
