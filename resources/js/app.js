22bc6125e1b6b62f4ce032543c5457d394f9c819
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-14 18:18:56 -0300

    adding jquery and datatables to laravel mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,10 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 import Swal from 'sweetalert2';
+import 'datatables.net-bs5';
+import $ from 'jquery';
 
+window.$ = window.jQuery = $;
 window.Alpine = Alpine;
 
 Alpine.start();
