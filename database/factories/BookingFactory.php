d57b473be42b697d479a130d8d5ae0f90b2490e1
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-06-15 01:37:15 -0300

    minor fix at times in booking factory

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -6,6 +6,7 @@ use App\Models\Assignment;
 use App\Models\Classroom;
 use App\Models\Event;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,18 +21,19 @@ class BookingFactory extends Factory
      */
     public function definition()
     {
-        
+        $start = $this->faker->time('H:i:s', '20:00:00');
+
         return [
             'user_id' => User::all()->random()->id,
             'classroom_id' => Classroom::all()->random()->id,
             'assignment_id' => Assignment::all()->random()->id,
             'event_id' => Event::all()->random()->id,
-            'description' => $this->faker->sentence($nbWords=4,$variableNbWords=true),
-            'status' => $this->faker->randomElement(['pending','in_progress','finished']),
-            'week_day' =>$this->faker->randomElement(['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']),
-            'booking_date' =>$this->faker->dateTimeThisYear('+2 months'),
-            'start_time' =>$this->faker->time($format='H:i:s'),
-            'finish_time' =>$this->faker->time($format='H:i:s')
+            'description' => $this->faker->sentence($nbWords = 4, $variableNbWords = true),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'finished']),
+            'week_day' => $this->faker->randomElement(['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', NULL]),
+            'booking_date' => $this->faker->dateTimeThisYear('+2 months'),
+            'start_time' => $start,
+            'finish_time' => Carbon::createFromTimeString($start)->addHours(rand(1, 3)),
         ];
     }
 }
