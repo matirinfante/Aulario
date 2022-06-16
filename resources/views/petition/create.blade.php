d8ecfa9c00d99cbd73b20b1593d37d502ac9a962
AndyVil <lucandvil@gmail.com>
2022-06-16 19:37:33 -0300

    Petitions changes

--- resources/views/petition/create.blade.php
+++ resources/views/petition/create.blade.php
@@ -1,83 +1,76 @@
-@extends('layouts.app')
-
-@section('content')
-    <style>
-        .alerta {
-            padding: 3px;
-            font-size: 14px;
-            color: #842029;
-            background: #f8d7da;
-            border: solid 1px #f5c2c7;
-            border-radius: 5px;
-            margin-top: 3px;
-            width: 100%;
-        }
-
-    </style>
-
-<a class="btn btn-outline-dark" href="{{ url('petitions') }}" role="button" style="margin-left: 1%">Listado de Usuarios</a>
-    <div id="container">
-        <h3 class="text-center m-4">Creación de Usuario</h3>
-        <form id="form" class="form_style" method="POST" action="{{route('petitions.store')}}">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">Nombre</label>
-                <input type="text" class="form-control" name="name" id="name" value="{{$user->name}} {{$user->surname}}" disabled>
+<div class="modal fade" id="createModal{{ $user }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Datalles de Reserva</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="mb-3">
-                <input type="hidden" class="form-control" name="user_id" id="user_id" value="{{$user->id}}">
-            </div>
-            <div class="mb-3">
-                <label for="assignment_id" class="form-label">Materias</label>
-                <select name="assignment_id" class="form-select select2-user" aria-label="Materia" style="width: 100%">
-                    <option value="-1" disabled></option>
-                    @foreach ($assignments as $assignment)
-                        <option value="{{ $assignment->id }}">
+            <div class="modal-body">
+                <div id="container">
+                    <h3 class="text-center m-4">Crear Petición</h3>
+                    <form id="form" class="form_style" method="POST" action="{{route('petitions.store')}}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" name="name" id="name" value="{{$user->name}} {{$user->surname}}" disabled>
+                        </div>
+                        <div class="mb-3">
+                            <input type="hidden" class="form-control" name="user_id" id="user_id" value="{{$user->id}}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="assignment_id" class="form-label">Materias</label>
+                            {{-- <select name="assignment_id" class="form-select select2-user" aria-label="Materia" style="width: 100%">
+                                <option value="-1" disabled></option>
+                                @foreach ($assignments as $assignment)
+                                <option value="{{ $assignment->id }}">
                             {{ $assignment->assignment_name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="estimated_people" class="form-label">Cantidad alumnos</label>
-                <input type="text" class="form-control" name="estimated_people" id="estimated_people">
-            </div>
-            <div class="mb-3">
-                <label for="classroom_type" class="form-label">Tipo Aula</label>
-                <input type="text" class="form-control" name="classroom_type" id="classroom_type" value="Híbrido">
-            </div>
-            <div class="mb-3">
-                <label for="start_date" class="form-label">Fecha Inicio</label>
-                <input type="date" class="form-control" name="start_date" id="start_date">
-            </div>
-            <div class="mb-3">
-                <label for="finish_date" class="form-label">Fecha Fin</label>
-                <input type="date" class="form-control" name="finish_date" id="finish_date">
-            </div>
-            <div class="mb-3">
-                <label for="start_time" class="form-label">Hora Inicio</label>
-                <input type="time" class="form-control" name="start_time" id="start_time">
-            </div>
-            <div class="mb-3">
-                <label for="finish_time" class="form-label">Hora Fin</label>
-                <input type="time" class="form-control" name="finish_time" id="finish_time">
-            </div>
-            <div class="mb-3">
-                <label for="days" class="form-label">Dia</label>
-                <input type="text" class="form-control" name="days" id="days">
-            </div>
-            <div class="mb-3">
-                <label for="message" class="form-label">Mensaje</label>
-                <input type="text" class="form-control" name="message" id="message">
-            </div>
+                            </option>
+                            @endforeach
+                            </select> --}}
+                        </div>
+                        <div class="mb-3">
+                            <label for="estimated_people" class="form-label">Cantidad alumnos</label>
+                            <input type="text" class="form-control" name="estimated_people" id="estimated_people">
+                        </div>
+                        <div class="mb-3">
+                            <label for="classroom_type" class="form-label">Tipo Aula</label>
+                            <input type="text" class="form-control" name="classroom_type" id="classroom_type" value="Híbrido">
+                        </div>
+                        <div class="mb-3">
+                            <label for="start_date" class="form-label">Fecha Inicio</label>
+                            <input type="date" class="form-control" name="start_date" id="start_date">
+                        </div>
+                        <div class="mb-3">
+                            <label for="finish_date" class="form-label">Fecha Fin</label>
+                            <input type="date" class="form-control" name="finish_date" id="finish_date">
+                        </div>
+                        <div class="mb-3">
+                            <label for="start_time" class="form-label">Hora Inicio</label>
+                            <input type="time" class="form-control" name="start_time" id="start_time">
+                        </div>
+                        <div class="mb-3">
+                            <label for="finish_time" class="form-label">Hora Fin</label>
+                            <input type="time" class="form-control" name="finish_time" id="finish_time">
+                        </div>
+                        <div class="mb-3">
+                            <label for="days" class="form-label">Dia</label>
+                            <input type="text" class="form-control" name="days" id="days">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message" class="form-label">Mensaje</label>
+                            <input type="text" class="form-control" name="message" id="message">
+                        </div>
 
-            <button id="submit" type="submit" class="btn btn-primary">Crear</button>
-        </form>
+                        <button id="submit" type="submit" class="btn btn-primary">Crear</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/validator@latest/validator.min.js"></script>
-    {{-- <script type="text/javascript">
+</div>
+
+
+{{-- <script type="text/javascript">
        let d = document
         const $inputName = d.getElementById('name'),
             $inputSurname = d.getElementById('surname'),
@@ -173,5 +166,7 @@
                 $button.classList.remove('disabled')
             }
         })
-    </script> --}}
-@endsection
+</script> --}}
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/validator@latest/validator.min.js"></script>
