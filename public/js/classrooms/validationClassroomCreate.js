9566bc162f967022e0f696ee5ba80a1f64f187a2
Rocio Graff <rocio.graff@est.fi.uncoma.edu.ar>
2022-06-16 23:49:27 -0300

    correcciones validacion crear aula

--- public/js/classrooms/validationClassroomCreate.js
+++ public/js/classrooms/validationClassroomCreate.js
@@ -1,12 +1,12 @@
 let d = document
-const $inputName = d.getElementById('classroom_name'),
-    $inputLocation = d.getElementById('location'),
-    $inputCapacity = d.getElementById('capacity'),
-    $button = d.getElementById('submit'),
-    $errorName = d.getElementById('errorClassroomName'),
-    $errorLocation = d.getElementById('errorLocation'),
-    $errorCapacity = d.getElementById('errorCapacity'),
-    form = d.getElementById('form_classroom')
+const $inputName = d.getElementById('classroom_name_create'),
+    $inputLocation = d.getElementById('location_create'),
+    $inputCapacity = d.getElementById('capacity_create'),
+    $button = d.getElementById('submit_create'),
+    $errorName = d.getElementById('errorClassroomNameCreate'),
+    $errorLocation = d.getElementById('errorLocationCreate'),
+    $errorCapacity = d.getElementById('errorCapacityCreate'),
+    form = d.getElementById('form_classroom_create')
 let v1 = false,
     v2 = false,
     v3 = false
@@ -14,29 +14,16 @@ let v1 = false,
 form.addEventListener('click', e => {
     //Validamos que el nombre sea letras y no esté vacio
     if (!validator.isEmpty($inputName.value)) {
-        if (validator.isAlpha($inputName.value)) {
-            console.log('goasldasd')
-            v1 = true
-            $errorName.classList.add('d-none')
-        } else {
-            $errorName.textContent = 'El nombre contiene numeros'
-            $errorName.classList.remove('d-none')
-        }
+        v1 = true
+        $errorName.classList.add('d-none')
     } else {
         $errorName.textContent = 'El nombre está vacio'
         $errorName.classList.remove('d-none')
     }
     //Validamos que la locacion sea letras y no esté vacia
     if (!validator.isEmpty($inputLocation.value)) {
-        if (validator.isAlpha($inputLocation.value)) {
-            console.log('entre y servi')
-            v2 = true
-            $errorLocation.classList.add('d-none')
-        } else {
-            console.log('entre ')
-            $errorLocation.textContent = 'La locación contiene números'
-            $errorLocation.classList.remove('d-none')
-        }
+        v2 = true
+        $errorLocation.classList.add('d-none')
     } else {
         $errorLocation.textContent = 'La locación está vacia'
         $errorLocation.classList.remove('d-none')
@@ -44,7 +31,7 @@ form.addEventListener('click', e => {
     //Validamos que la capacidad sea numeros, no esté vacia y que el valor esté entre el 1 y los 200
     if (!validator.isEmpty($inputCapacity.value)) {
         if (validator.isNumeric($inputCapacity.value)) {
-            if ($inputCapacity.value < 1 && $inputCapacity.value > 200) {
+            if ($inputCapacity.value < 200 && $inputCapacity.value > 1) {
                 v3 = true
                 $errorCapacity.classList.add('d-none')
             } else {
@@ -56,11 +43,10 @@ form.addEventListener('click', e => {
             $errorCapacity.classList.remove('d-none')
         }
     } else {
-        console.log('el dni esta vacio')
         $errorCapacity.textContent = 'La capacidad está vacia'
         $errorCapacity.classList.remove('d-none')
     }
-    
+
 
     if (v1 && v2 && v3) {
         $button.classList.remove('disabled')
