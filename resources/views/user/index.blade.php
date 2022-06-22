497e33cc8713eae624ab116ef94fbf77a3464eca
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-06-21 22:41:50 -0300

    (users view) icon and status column removed

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -24,7 +24,6 @@
                 <tr>
                     <td>Nombre</td>
                     <td>Email</td>
-                    <td>Estado</td>
                     <td>Accion</td>
                     <td>Cambiar estado</td>
                 </tr>
@@ -34,23 +33,6 @@
                     <tr>
                         <td> {{$user['name']}} {{$user['surname']}}</td>
                         <td>{{$user['email']}}</td>
-                        <td  data-statusSvg="{{ $user->id }}">
-                            @if (!($user->trashed()))
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#1f9b08"
-                                    class="bi bi-check-circle" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                    <path
-                                        d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
-                                </svg>
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#9b0808"
-                                    class="bi bi-x-circle" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                                    <path
-                                        d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                </svg>
-                            @endif
-                        </td>
                         <td>
                             {{-- Ver Usuario --}}
                             <a class="btn btn-primary btn-sm" style="pointer-events: auto;" onclick="seeUser({{$user}})">Ver</a>
@@ -72,7 +54,7 @@
                         </td>
                     </tr>
                 @empty
-                    <td colspan="5">No hay registros</td>
+                    <td colspan="4">No hay registros</td>
                 @endforelse
                 </tbody>
             </table>
