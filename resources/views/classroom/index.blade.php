e3292806ebd31179524415082942f119a96a5653
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-06-21 23:43:19 -0300

    permissions on classrooms view added

--- resources/views/classroom/index.blade.php
+++ resources/views/classroom/index.blade.php
@@ -10,11 +10,11 @@
     <h3 class="text-center m-4">Listado de Aulas</h3>
 
     {{-- Mensaje de error --}}
-    @if($errors->any())
+    @if ($errors->any())
         <div class="alert alert-danger d-none" id="errorsMsj" role="alert">
 
-            @foreach($errors->all() as $error)
-                {{ $error }}<br/>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br />
             @endforeach
         </div>
     @endif
@@ -22,70 +22,86 @@
     <div class="card" style="width: 1000px; margin: auto;">
         <div class="card-body">
             <table class="table table-striped table-hover" id="classroom">
-                <button type="" class="btn btn-success m-3 btn-sm" data-bs-toggle="modal" data-bs-target="#createModal"
-                        id="buttonCreate">Crear Aula
-                </button>
+                @can('create classrooms')
+                    <button type="" class="btn btn-success m-3 btn-sm" data-bs-toggle="modal"
+                        data-bs-target="#createModal" id="buttonCreate">Crear Aula
+                    </button>
+                @endcan
                 <thead class="bg-secondary text-light">
-                <tr>
-                    <td>Nombre</td>
-                    <td>Edificio</td>
-                    <td>Capacidad</td>
-                    <td>Tipo de aula</td>
-                    <td>Acción</td>
-                    <td>Estado</td>
-                </tr>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Edificio</td>
+                        <td>Capacidad</td>
+                        <td>Tipo de aula</td>
+                        @canany(['show classrooms', 'edit classrooms'])
+                            <td>Acción</td>
+                        @endcanany
+                        @can('delete classrooms')
+                            <td>Estado</td>
+                        @endcan
+                    </tr>
                 </thead>
                 <tbody>
-                @forelse ($classrooms as $class)
-                    <tr>
-                        <td> {{ $class->classroom_name }} </td>
-                        <td>{{ $class->building }}</td>
-                        <td>{{ $class->capacity }}</td>
-                        <td>{{ $class->type }}</td>
-                        <td>
-                            {{-- Boton Ver --}}
-                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                                    data-bs-target="#viewModal{{ $class->id }}">Ver
-                            </button>
-                            {{-- Modal Ver --}}
-                            @include('classroom.show', ['classroom' => $class])
+                    @forelse ($classrooms as $class)
+                        <tr>
+                            <td> {{ $class->classroom_name }} </td>
+                            <td>{{ $class->building }}</td>
+                            <td>{{ $class->capacity }}</td>
+                            <td>{{ $class->type }}</td>
+                            @canany(['show classrooms', 'edit classrooms'])
+                                <td>
+                                    @can('show classrooms')
+                                        {{-- Boton Ver --}}
+                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                            data-bs-target="#viewModal{{ $class->id }}">Ver
+                                        </button>
+                                        {{-- Modal Ver --}}
+                                        @include('classroom.show', ['classroom' => $class])
+                                    @endcan
 
-                            {{-- Boton editar --}}
-                            @if ($class->deleted_at == null)
-                                <button type="button" id="buttonEdit{{ $class->id }}"
-                                        class="btn btn-secondary btn-sm" data-bs-toggle="modal"
-                                        data-bs-target="#updateModal{{ $class->id }}"
-                                        onclick="precargarSelect({{ $class }});">Editar
-                                </button>
-                            @else
-                                <button type="button" id="buttonEdit{{ $class->id }}"
-                                        class="btn btn-secondary btn-sm disabled" data-bs-toggle="modal"
-                                        data-bs-target="#updateModal{{ $class->id }}">Editar
-                                </button>
-                        @endif
-                        {{-- Modal Editar --}}
-                        @include('classroom.edit', ['classroom' => $class])
+                                    @can('edit classrooms')
+                                        {{-- Boton editar --}}
+                                        @if ($class->deleted_at == null)
+                                            <button type="button" id="buttonEdit{{ $class->id }}"
+                                                class="btn btn-secondary btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#updateModal{{ $class->id }}"
+                                                onclick="precargarSelect({{ $class }});">Editar
+                                            </button>
+                                        @else
+                                            <button type="button" id="buttonEdit{{ $class->id }}"
+                                                class="btn btn-secondary btn-sm disabled" data-bs-toggle="modal"
+                                                data-bs-target="#updateModal{{ $class->id }}">Editar
+                                            </button>
+                                        @endif
+                                        {{-- Modal Editar --}}
+                                        @include('classroom.edit', ['classroom' => $class])
+                                    @endcan
 
-                        {{-- Boton Deshabilitar/Habilitar --}}
-                        <td>
-                            <div class="form-check form-switch">
-                                <input data-id="{{ $class->id }}" data-token="{{ csrf_token() }}"
-                                       class="form-check-input activeSwitch" type="checkbox" role="switch"
-                                    {{ !$class->trashed() ? 'checked' : '' }}>
-                            </div>
-                        </td>
-                        </td>
-                    </tr>
-                @empty
-                    <td colspan="8" class="text-center text-secondary">No hay registros</td>
-                @endforelse
+                                </td>
+                            @endcanany
+                            @can('delete classrooms')
+                                <td>
+                                    {{-- Boton Deshabilitar/Habilitar --}}
+                                    <div class="form-check form-switch">
+                                        <input data-id="{{ $class->id }}" data-token="{{ csrf_token() }}"
+                                            class="form-check-input activeSwitch" type="checkbox" role="switch"
+                                            {{ !$class->trashed() ? 'checked' : '' }}>
+                                    </div>
+                                </td>
+                            @endcan
+                        </tr>
+                    @empty
+                        <td colspan="8" class="text-center text-secondary">No hay registros</td>
+                    @endforelse
                 </tbody>
             </table>
         </div>
     </div>
 
-    <!-- Modal Crear-->
-    @include('classroom.create')
+    @can('create classrooms')
+        <!-- Modal Crear-->
+        @include('classroom.create')
+    @endcan
 
 @endsection
 @section('scripts')
