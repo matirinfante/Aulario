132c4bc17b09a152eac615ba92262a390c7f8944
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-06-23 05:32:42 -0300

    logbook datatable with pdf download option

--- resources/views/logbook/index.blade.php
+++ resources/views/logbook/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('styles')
+    <style>
+        /* estilo para boton de descarga (pdf) */
+        button.btn.btn-secondary.buttons-pdf.buttons-html5 {
+            background-color: #c7be57;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -8,6 +14,7 @@
 
     <div class="card m-auto mt-3" style="width: 1000px;">
         <div class="card-body">
+
             <table class="table table-striped table-hover" id="logbooks">
 
                 <thead class="bg-secondary text-light">
@@ -24,29 +31,33 @@
                     @forelse ($today_logbook as $logbook)
                         <tr>
                             {{-- nombre de la reserva (materia o evento) --}}
-                            @isset($logbook->booking->assignment)
+                            @isset($logbook->booking->assignment->assignment_name)
                                 <td>{{ $logbook->booking->assignment->assignment_name }}</td>
                             @endisset
-                            @isset($logbook->booking->event)
+                            @isset($logbook->booking->event->event_name)
                                 <td>{{ $logbook->booking->event->event_name }}</td>
                             @endisset
 
                             {{-- fecha de la reserva --}}
-                            <td>{{ $logbook->date }}</td>
+                            <td>{{ date('d/m/Y', strtotime($logbook->date)) }}</td>
 
                             {{-- nombre completo de usuario --}}
-                            <td>{{ $logbook->user->name }}, {{ $logbook->user->surname }}</td>
+                            @isset($logbook->user->name, $logbook->user->surname)
+                                <td>{{ $logbook->user->name }}, {{ $logbook->user->surname }}</td>
+                            @else
+                                <td>No disponible</td>
+                            @endisset
 
                             {{-- hora de ingreso --}}
                             @isset($logbook->check_in)
-                                <td>{{ $logbook->check_in }}</td>
+                                <td>{{ date('h:i', strtotime($logbook->check_in)) }}</td>
                             @else
                                 <td>No disponible</td>
                             @endisset
 
                             {{-- hora de salida --}}
                             @isset($logbook->check_out)
-                                <td>{{ $logbook->check_out }}</td>
+                                <td>{{ date('h:i', strtotime($logbook->check_out)) }}</td>
                             @else
                                 <td>No disponible</td>
                             @endisset
