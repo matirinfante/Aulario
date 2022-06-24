029ad237699a2868d7ebbf2bf00fba104cb3d661
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-06-24 01:28:08 -0300

    goTop function fix for mobile

--- public/js/toTop.js
+++ public/js/toTop.js
@@ -2,8 +2,7 @@
 function scrollUp(){
     var $currentScroll = document.documentElement.scrollTop; // Obtiene el número de pixels desplazados
     if($currentScroll > 0){
-        window.requestAnimationFrame(scrollUp); // window.requestAnimationFrame informa al navegador que quieres realizar una animación y solicita que el navegador programe el repintado de la ventana para el próximo ciclo de animación. El método acepta como argumento una función a la que llamar antes de efectuar el repintado.
-        window.scrollTo(0, $currentScroll - ($currentScroll / 15)); // Velocidad de scroll
+        $('html, body').animate({scrollTop: '0px'}, 500);
     }
 }
 
