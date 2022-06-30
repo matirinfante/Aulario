4f3a6700f8f1f1e8a64534863a68070b60c1c018
Eluney Salvaro <eluneyjsalvaro@gmail.com>
2022-06-30 17:18:11 -0300

    validations in changepassword and flash are solved

--- public/js/users/userProfile.js
+++ public/js/users/userProfile.js
@@ -10,3 +10,65 @@ function showPassword(elem) {
         document.getElementById("hidden_" + elem).style.visibility = 'visible';
     }
 }
+
+
+const d = document
+let $btnSubmit = d.getElementById('btnSubmit'),
+    $form = d.getElementById('formChangePassword'),
+
+    $errorNewPass = d.getElementById('errorNewPassword'),
+    $errorOldPass = d.getElementById('errorOldPassword'),
+    $errorNewPassConfirmation = d.getElementById('errorNewPasswordConfirmation')
+
+
+$btnSubmit.addEventListener('click', e => {
+    let oldPass = d.getElementById('old_password').value
+    let newPass = d.getElementById('new_password').value
+    let newPassConfirmation = d.getElementById('new_password_confirmation').value
+    let v1 = false
+    let v2 = false
+    let v3 = false
+    let v4 = false
+    e.preventDefault()
+
+    if (newPass.length > 0) {
+        v2 = true
+            // $errorNewPass.classList.add("d-none")
+    } else {
+        $errorNewPass.classList.remove("d-none")
+        $errorNewPass.textContent = 'Ingrese un valor'
+    }
+
+    if (oldPass.length > 0) {
+        v1 = true
+        $errorOldPass.classList.add("d-none")
+    } else {
+        $errorOldPass.textContent = 'Ingrese un valor'
+        $errorOldPass.classList.remove("d-none")
+    }
+
+
+    if (newPassConfirmation.length > 0) {
+        v3 = true
+            // $errorNewPassConfirmation.classList.add("d-none")
+    } else {
+        $errorNewPassConfirmation.textContent = 'Ingrese un valor'
+        $errorNewPassConfirmation.classList.remove("d-none")
+    }
+
+    if (newPass === newPassConfirmation) {
+        v4 = true
+        $errorNewPass.classList.add('d-none')
+        $errorNewPassConfirmation.classList.add('d-none')
+    } else {
+        $errorNewPass.textContent = 'La contraseña nueva no son iguales'
+        $errorNewPass.classList.remove('d-none')
+        $errorNewPassConfirmation.textContent = 'La contraseña nueva no son iguales'
+        $errorNewPassConfirmation.classList.remove('d-none')
+    }
+
+    if (v1 && v2 && v3 && v4) {
+        $form.submit()
+    }
+
+})
