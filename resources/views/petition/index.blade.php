69513812c753e7a55f3d2b4d866beffa1f4c7a62
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-07-01 18:36:49 -0300

    petition index view without datatables

--- resources/views/petition/index.blade.php
+++ resources/views/petition/index.blade.php
@@ -8,37 +8,38 @@
 
     {{-- Mensaje de error --}}
     @if ($errors->any())
-    <div class="alert alert-danger d-none" id="errorsMsj" role="alert">
+        <div class="alert alert-danger d-none" id="errorsMsj" role="alert">
 
-        @foreach ($errors->all() as $error)
-        {{ $error }}
-        <br/>
-        @endforeach
-    </div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}
+                <br/>
+            @endforeach
+        </div>
     @endif
 
     <div class="container-fluid">
         @hasanyrole('teacher')
-            <h3 class="text-center m-4">Mis Peticiones</h3>
+        <h3 class="text-center m-4">Mis Peticiones</h3>
         @else
             <h3 class="text-center m-4">Listado de Peticiones</h3>
-        @endhasanyrole
-        <div>
-            <div class="row" align=center>
-                <div class="col-12 pb-3">
-                    @can('create petitions')
-                    <!-- Traemos el usuario al cual le vamos a sacar la inf. Es un obj. -->
-                    <button type='button' class='btn btn-primary' data-bs-toggle="modal" data-bs-target="#createModal{{ auth()->user()->id }}">Crear nueva petición
-                    </button>
-                    @include('petition.create', ['user' => auth()->user()])
-                    @endcan
+            @endhasanyrole
+            <div>
+                <div class="row" align=center>
+                    <div class="col-12 pb-3">
+                        @can('create petitions')
+                            <!-- Traemos el usuario al cual le vamos a sacar la inf. Es un obj. -->
+                            <button type='button' class='btn btn-primary' data-bs-toggle="modal"
+                                    data-bs-target="#createModal{{ auth()->user()->id }}">Crear nueva petición
+                            </button>
+                            @include('petition.create', ['user' => auth()->user()])
+                        @endcan
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="card" style="width: 1000px; margin: auto;">
-            <div class="card-body">
-                <table class="table table-striped table-hover" id="petitions">
-                    <thead class="bg-secondary text-light">
+            <div class="card" style="width: 1000px; margin: auto;">
+                <div class="card-body">
+                    <table class="table table-striped table-hover" id="petitions">
+                        <thead class="bg-secondary text-light">
                         <tr>
                             <td>Usuario</td>
                             <td>Materia</td>
@@ -52,77 +53,85 @@
                                 <td></td>
                             @endcanany
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         @forelse ($petitions as $petition)
-                        <tr>
-                            <td style="vertical-align: middle;"> {{ $petition->user->name }}
-                                {{ $petition->user->surname }}
-                            </td>
-                            <td style="vertical-align: middle;"> {{ $petition->assignment->assignment_name }}</td>
-                            <td style="vertical-align: middle;"> {{ $petition['estimated_people'] }}</td>
-                            <td style="vertical-align: middle;"> {{ $petition['classroom_type'] }} </td>
-                            <td style="vertical-align: middle;">
-                                @if ($petition['status'] == 'unsolved')
-                                <span class="badge bg-warning"> Sin resolver </span>
-                                @elseif ($petition['status'] == 'rejected')
-                                <span class="badge bg-danger"> Rechazada </span>
-                                @else
-                                <span class="badge bg-success"> Aceptada </span>
-                                @endif
-                            </td>
-                            @can('show petitions')
-                            <td style="vertical-align: middle; text-align: center;">
-                                <!-- Boton Modal Detalles -->
-                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#viewModal{{ $petition['id'] }}">Detalles
-                                </button>
-                                @include('petition.show', ['petition' => $petition])
-                            </td>
-                            @endcan
-                            @canany(['reject petitions', 'accept petitions'])
-                            <td style="vertical-align: middle; text-align: center;">
-                                @if ($petition['status'] == 'unsolved')
-                                <div class="container">
-                                    @can('accept petitions')
-                                    <!-- Boton Aceptar Peticion -->
-                                    <div align='center' style="margin-bottom: 5px;">
-                                        <form method="get" action="{{ route('bookings.petition') }}">
-                                            <input hidden value="{{ $petition->id }}" name="id">
-                                            <button type="submit" class="btn btn-success btn-sm" data-bs-target="{{ $petition['id'] }}">Asignar aulas
-                                            </button>
-                                        </form>
-                                        <p class="alerta d-none" id="errorMessageCreate">Error</p>
-                                    </div>
-                                    @endcan
-                                    @can('delete petitions')
-                                    <!-- Boton Rechazar Peticion -->
-                                    <div align='center'>
-                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#rejectedModal{{ $petition['id'] }}">Rechazar
+                            <tr>
+                                <td style="vertical-align: middle;"> {{ $petition->user->name }}
+                                    {{ $petition->user->surname }}
+                                </td>
+                                <td style="vertical-align: middle;"> {{ $petition->assignment->assignment_name }}</td>
+                                <td style="vertical-align: middle;"> {{ $petition['estimated_people'] }}</td>
+                                <td style="vertical-align: middle;"> {{ $petition['classroom_type'] }} </td>
+                                <td style="vertical-align: middle;">
+                                    @if ($petition['status'] == 'unsolved')
+                                        <span class="badge bg-warning"> Sin resolver </span>
+                                    @elseif ($petition['status'] == 'rejected')
+                                        <span class="badge bg-danger"> Rechazada </span>
+                                    @else
+                                        <span class="badge bg-success"> Aceptada </span>
+                                    @endif
+                                </td>
+                                @can('show petitions')
+                                    <td style="vertical-align: middle; text-align: center;">
+                                        <!-- Boton Modal Detalles -->
+                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#viewModal{{ $petition['id'] }}">Detalles
                                         </button>
-                                        <p class="alerta d-none" id="errorMessageCreate">Error</p>
-                                    </div>
-                                    @endcan
-                                </div>
-                                @can('delete petitions')
-                                <!-- Modal Rechazar Detalles -->
-                                @include('petition.edit', ['petition' => $petition])
+                                        @include('petition.show', ['petition' => $petition])
+                                    </td>
                                 @endcan
-                                @endif
-                            </td>
-                            @endcanany
-                        </tr>
+                                @canany(['reject petitions', 'accept petitions'])
+                                    <td style="vertical-align: middle; text-align: center;">
+                                        @if ($petition['status'] == 'unsolved')
+                                            <div class="container">
+                                                @can('accept petitions')
+                                                    <!-- Boton Aceptar Peticion -->
+                                                    <div align='center' style="margin-bottom: 5px;">
+                                                        <form method="get" action="{{ route('bookings.petition') }}">
+                                                            <input hidden value="{{ $petition->id }}" name="id">
+                                                            <button type="submit" class="btn btn-success btn-sm"
+                                                                    data-bs-target="{{ $petition['id'] }}">Asignar aulas
+                                                            </button>
+                                                        </form>
+                                                        <p class="alerta d-none" id="errorMessageCreate">Error</p>
+                                                    </div>
+                                                @endcan
+                                                @can('delete petitions')
+                                                    <!-- Boton Rechazar Peticion -->
+                                                    <div align='center'>
+                                                        <button type="button" class="btn btn-danger btn-sm"
+                                                                data-bs-toggle="modal"
+                                                                data-bs-target="#rejectedModal{{ $petition['id'] }}">
+                                                            Rechazar
+                                                        </button>
+                                                        <p class="alerta d-none" id="errorMessageCreate">Error</p>
+                                                    </div>
+                                                @endcan
+                                            </div>
+                                            @can('delete petitions')
+                                                <!-- Modal Rechazar Detalles -->
+                                                @include('petition.edit', ['petition' => $petition])
+                                            @endcan
+                                        @endif
+                                    </td>
+                                @endcanany
+                            </tr>
                         @empty
-                        @canany(['reject petitions', 'accept petitions'])
-                        <td class="text-center text-secondary" colspan="6">No hay registros</td>
-                        @else
-                        <td class="text-center text-secondary" colspan="7">No hay registros</td>
-                        @endcanany
+                            @canany(['reject petitions', 'accept petitions'])
+                                <td class="text-center text-secondary" colspan="6">No hay registros</td>
+                            @else
+                                <td class="text-center text-secondary" colspan="7">No hay registros</td>
+                            @endcanany
                         @endforelse
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
     </div>
+    <p id="warning_reason">
+
+    </p>
 
 @endsection
 
@@ -131,4 +140,5 @@
     <script src="{{ asset('js/petitions/sweetAlert.js') }}" defer></script>
     {{-- Comprobacion de formulario --}}
     <script src="{{ asset('js/petitions/petition_forms.js') }}" defer></script>
+
 @endsection
