c8b5d90cd0c814a131dcbd7f079f09232ca62b53
augusto <augusto.perceval@est.fi.uncoma.edu.ar>
2022-07-02 19:37:42 -0300

    manejo de errores

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,4 +2,7 @@
 
 @section('title', __('Not Found'))
 @section('code', '404')
+@section('image')
+<img src="{{asset('assets/img/404.svg')}}" style="height: 100vh;">
+@endsection
 @section('message', __('Not Found'))
