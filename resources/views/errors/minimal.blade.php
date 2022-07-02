4a8a34f0356afe92b365a23d68aa2a0390e5775b
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-07-02 05:30:36 -0300

    minor error pages changes

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -25,7 +25,7 @@
                     </div>
 
                     <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                        @yield('message') >:(
+                        @yield('message')
                     </div>
                 </div>
             </div>
