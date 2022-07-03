f80351b9d70fb95440540af70da0a2db03d00799
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2022-07-02 21:12:59 -0300

    custom errors (403, 404 & 503)

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,8 +1,13 @@
-@extends('errors::layout')
+@extends('errors::illustrated-layout')
 
 @section('title', __('Forbidden'))
 @section('code', '403')
 @section('image')
-<img src="{{asset('assets/img/403.png')}}" style="height: 100vh;">
+    <div style="background-image: url({{ asset('assets/img/403.jpg') }}); background-size: cover;"
+        class="absolute pin bg-no-repeat md:bg-left lg:bg-center">
+    </div>
 @endsection
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __($exception->getMessage() ?: 'Usted no tiene autorización para acceder a este apartado.'))
+
+
+@section('quote', 'Thank You for Your Cooperation, Good Night!')
